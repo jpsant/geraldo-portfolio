952c50cff2b95af0aa2e168e4c430c01c6f70c0b
João Paulo <j.paaulinho@gmail.com>
2019-10-07 21:27:19 -0300

    adicao das tooltips nas skills

--- src/containers/Skills/Skills.js
+++ src/containers/Skills/Skills.js
@@ -15,21 +15,48 @@ class Skills extends Component {
                         </div>
                         <div className={classes.skills}>
                             <Fade top cascade >
-                            <div className={classes.block1}>
-                                <div><img alt="" className={classes.skillIcon} src={require('./assets/icons/1.svg')}></img></div>
-                                <div><img alt="" className={classes.skillIcon} src={require('./assets/icons/2.svg')}></img></div>
-                                <div><img alt="" className={classes.skillIcon} src={require('./assets/icons/3.png')}></img></div>
-                            </div>
-                            <div className={classes.block2}>
-                                <div><img alt="" className={classes.skillIcon} src={require('./assets/icons/4.png')}></img></div>
-                                <div><img alt="" className={classes.skillIcon} src={require('./assets/icons/5.png')}></img></div>
-                                <div><img alt="" className={classes.skillIcon} src={require('./assets/icons/6.png')}></img></div>
-                            </div>
-                            <div className={classes.block3}>
-                                <div><img alt="" className={classes.skillIcon} src={require('./assets/icons/7.png')}></img></div>
-                                <div><img alt="" className={classes.skillIcon} src={require('./assets/icons/8.png')}></img></div>
-                                <div><img alt="" className={classes.skillIcon} src={require('./assets/icons/9.png')}></img></div>
-                            </div>
+                                <div className={classes.block1}>
+                                    <div className={classes.golang}>
+                                        <span className={classes.tooltipText}>Golang</span>
+                                        <img alt="" className={classes.skillIcon} src={require('./assets/icons/1.svg')}></img>
+                                    </div>
+                                    <div className={classes.docker}>
+                                        <span className={classes.tooltipText}>Docker</span>
+                                        <img alt="" className={classes.skillIcon} src={require('./assets/icons/2.svg')}></img>
+                                    </div>
+                                    <div className={classes.grpc}>
+                                        <span className={classes.tooltipText}>GRPC</span>
+                                        <img alt="" className={classes.skillIcon} src={require('./assets/icons/3.png')}></img>
+                                    </div>
+                                </div>
+                                <div className={classes.block2}>
+                                    <div className={classes.c}>
+                                        <span className={classes.tooltipText}>C++</span>
+                                        <img alt="" className={classes.skillIcon} src={require('./assets/icons/4.png')}></img>
+                                    </div>
+                                    <div className={classes.plant}>
+                                        <span className={classes.tooltipText}>Plantinha</span>
+                                        <img alt="" className={classes.skillIcon} src={require('./assets/icons/5.png')}></img>
+                                    </div>
+                                    <div className={classes.mysql}>
+                                        <span className={classes.tooltipText}>MySQL</span>
+                                        <img alt="" className={classes.skillIcon} src={require('./assets/icons/6.png')}></img>
+                                    </div>
+                                </div>
+                                <div className={classes.block3}>
+                                    <div className={classes.javascript}>
+                                        <span className={classes.tooltipText}>Javascript</span>
+                                        <img alt="" className={classes.skillIcon} src={require('./assets/icons/7.png')}></img>
+                                    </div>
+                                    <div className={classes.nodejs}>
+                                        <span className={classes.tooltipText}>NodeJs</span>
+                                        <img alt="" className={classes.nodeJs} src={require('./assets/icons/8.svg')}></img>
+                                    </div>
+                                    <div className={classes.otimizacao}>
+                                        <span className={classes.tooltipText}>Otimização</span>
+                                        <img alt="" className={classes.skillIcon} src={require('./assets/icons/9.svg')}></img>
+                                    </div>
+                                </div>
                             </Fade>
                         </div>
                     </div>
